eaf0435836492faf7317a368a98e39d58f6f4fbd
88072860 <sibulele88@icloud.com>
2023-02-27 12:58:50 -0800

    let it go

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,33 +1,30 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _strcpy - copie the string
- * @dest: array that returns
- * @src: the pointer that receive the string
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
  *
- * Return: the parameter dest.
+ * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int n = 0;
-	int i;
+	int len, i;
 
-	while (src[n] != '\0')
+	len = 0;
+
+	while (src[len] != '\0')
 	{
-		n++;
+		len++;
 	}
 
-	i = 0;
-	while (i < n && src[i] != '\0')
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
+	dest[i] = '\0';
 
-	i = i;
-	while (i <= n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
 	return (dest);
 }
